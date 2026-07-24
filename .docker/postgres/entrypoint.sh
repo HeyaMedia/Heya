dae0aade90ccd7bc5a8fc373895dfbc97944f027
Michael Karbowiak <michael@karbowiak.dk>
2026-07-24 15:17:03 +0200

    fix(postgres): upgrade option-only server commands

--- .docker/postgres/entrypoint.sh
+++ .docker/postgres/entrypoint.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 set -eu
 
+run_upgrade=no
 case "${1:-}" in
-    postgres)
-        case "${2:-}" in
-            --help|-\?|--version|-V)
-                ;;
-            *)
-                /usr/local/bin/heya-postgres-upgrade
-                ;;
-        esac
+    postgres|-*)
+        run_upgrade=yes
         ;;
 esac
+
+# Match the upstream entrypoint's help/version behavior. Kubernetes commonly
+# supplies only `-c ...` arguments; docker-entrypoint.sh later prepends
+# `postgres`, so those invocations must run the upgrade too.
+for arg in "$@"
+do
+    case "$arg" in
+        --help|-\?|--describe-config|--version|-V)
+            run_upgrade=no
+            break
+            ;;
+    esac
+done
+
+if [ "$run_upgrade" = yes ]; then
+    /usr/local/bin/heya-postgres-upgrade
+fi
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
